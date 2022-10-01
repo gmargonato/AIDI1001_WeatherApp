6d278a0ca35b2e311fa22716f4e461e66df3dbbb
Gabriel Margonato <gabrielmargonato@Gabriels-MacBook-Air.local>
2022-10-01 19:28:09 -0400

    first commit

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
+    return render_template('index.html')   
+
+@app.route('/results', methods=['POST'])
+def render_results():
     
-    city_name = 'Toronto'
+    city_name = request.form['city_name']
     my_api_key = '974b6905630d60c93952b5f4679b3da1'
     r = requests.get('https://api.openweathermap.org/data/2.5/weather?q='+city_name+'&appid='+my_api_key)
 
@@ -18,8 +22,7 @@ def index():
     temperature = str(int(json_object['main']['temp']-273.15)) #converts from kelvin    
     condition = str(json_object['weather'][0]['main']        )
     
-    return 'Currently the weather in '+city_name+' is '+temperature+' degrees and the sky is '+condition
+    return 'Currently, the weather in '+city_name+' is '+temperature+' degrees and the sky is '+condition
 
-
-#app.run(host='0.0.0.0', port=81, debug=True, use_reloader=True)
-app.run()
+app.run(host='0.0.0.0', port=81, debug=True, use_reloader=True)
+#app.run()
